f2a7de238ee340dfba0f47c848f38f2a9817b911
luizcampos331 <luizcampos331@gmail.com>
2020-10-11 14:55:41 -0300

    challenge concluded

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,66 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const checkCustomer = await this.customersRepository.findById(customer_id);
+
+    if (!checkCustomer) {
+      throw new AppError('Customer not found');
+    }
+
+    const checkProducts = await this.productsRepository.findAllById(products);
+
+    if (!checkProducts.length) {
+      throw new AppError('Products not found');
+    }
+
+    const checkProductsIds = checkProducts.map(product => product.id);
+
+    const checkInexistsProducts = products.filter(
+      product => !checkProductsIds.includes(product.id),
+    );
+
+    if (checkInexistsProducts.length) {
+      throw new AppError('At least one product was not found');
+    }
+
+    const checkQuantityProducts = products.filter(
+      (product, index) => product.quantity > checkProducts[index].quantity,
+    );
+
+    if (checkQuantityProducts.length) {
+      throw new AppError('At least one product has insufficient quantity');
+    }
+
+    const completeProducts = products.map((product, index) => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: checkProducts[index].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: checkCustomer,
+      products: completeProducts,
+    });
+
+    const updatedQuantity = products.map((product, index) => ({
+      id: product.id,
+      quantity: checkProducts[index].quantity - product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(updatedQuantity);
+
+    return order;
   }
 }
 
